6932c3b4329bb768986b8fb54a103d5338391841
yuzeh <delta1080@gmail.com>
2017-09-14 11:36:19 -0700

    First commit

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+die() {
+  echo "${@}"
+  exit 1
+}
+
+dot_files_dir=`dirname $0`
+
+set -e
+
+[ `pwd` = "$HOME" ] || die "Please run this script from your home directory!"
+
+# Install OMZ
+if ! [ -d "${HOME}/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  rm "${HOME}/.zshrc"
+fi
+
+for f in .zshrc .zshenv .vim .vimrc .tmux .tmux.conf; do
+  [ -f $f ] || [ -d $f ] && die "Please remove ${f}!"
+done
+
+for f in .zshrc .zshenv .vim .tmux; do
+  src="${dot_files_dir}/${f}"
+  dst="${HOME}/${f}"
+  ln -s $src $dst
+  [ -f "${dst}/bootstrap.sh" ] && "${dst}/bootstrap.sh"
+done
